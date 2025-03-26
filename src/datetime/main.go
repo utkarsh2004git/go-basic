f1077aedc69dbb122de875f630730c121ce1a152
ukarsh2004git <utkarshshahare2004@gmail.com>
2025-03-26 21:55:53 +0530

    date-time

--- src/datetime/main.go
+++ src/datetime/main.go
@@ -1,8 +1,31 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
 	//yyyy-mm-dd
 	//2006-01-02 15:04:05
 	// 2 Jan 2006 , 3:04:05 PM
+
+	currentTime := time.Now().Local()
+
+	fmt.Println("current ", currentTime)
+
+	formattedDate := currentTime.Format("02-01-2006, Mon 03:04:05 PM")
+	fmt.Println(formattedDate)
+
+	dateStr := "2004-05-19"
+
+	newFormatTime, _ := time.Parse("2006-01-02", dateStr)
+	fmt.Println(newFormatTime)
+	fmt.Println(newFormatTime.Format("02-01-2006"))
+
+	new_date := currentTime.Add(50 * time.Minute)
+
+	fmt.Println("current ", currentTime)
+	fmt.Println(new_date)
 
 }
